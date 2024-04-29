f9c67ffdcd3040978b502d9501d91237a93cb3b2
obay-konbos <149474331+obay-konbos@users.noreply.github.com>
2024-04-29 19:02:59 +0400

    Add files via upload

---
+++ main1.js
@@ -0,0 +1,247 @@
+let all_product=document.getElementById("y")
+let list=document.getElementById("list")
+all_product.onmouseover=function(){
+    list.classList.remove("hide")
+}
+all_product.onmouseout=function(){
+    list.classList.add("hide")
+}
+list.onmouseover=function(){
+    list.classList.remove("hide")
+}
+list.onmouseout=function(){
+    list.classList.add("hide")
+}
+
+
+let v1_g=document.getElementById("ic1")
+let v1_r=document.getElementById("ic2")
+let c1_g=document.getElementById("ic3")
+let c1_gr=document.getElementById("ic4")
+let num=document.querySelector(".num")
+let n=0
+c1_g.addEventListener("click",() =>{
+    n--;
+    num.innerText = n;
+})
+c1_gr.addEventListener("click",() =>{
+    n++;
+    num.innerText = n;
+})
+v1_g.onclick=function(){
+    v1_g.classList.add("hide")
+    v1_r.classList.remove("hide")
+}
+v1_r.onclick=function(){
+    v1_r.classList.add("hide")
+    v1_g.classList.remove("hide")
+}
+c1_g.onclick=function(){
+    c1_g.classList.add("hide")
+    c1_gr.classList.remove("hide")
+}
+c1_gr.onclick=function(){
+    c1_gr.classList.add("hide")
+    c1_g.classList.remove("hide")
+}
+let v2_g=document.getElementById("ic5")
+let v2_r=document.getElementById("ic6")
+let c2_g=document.getElementById("ic7")
+let c2_gr=document.getElementById("ic8")
+c2_g.addEventListener("click",() =>{
+    n--;
+    num.innerText = n;
+})
+c2_gr.addEventListener("click",() =>{
+    n++;
+    num.innerText = n;
+})
+v2_g.onclick=function(){
+    v2_g.classList.add("hide")
+    v2_r.classList.remove("hide")
+}
+v2_r.onclick=function(){
+    v2_r.classList.add("hide")
+    v2_g.classList.remove("hide")
+}
+c2_g.onclick=function(){
+    c2_g.classList.add("hide")
+    c2_gr.classList.remove("hide")
+}
+c2_gr.onclick=function(){
+    c2_gr.classList.add("hide")
+    c2_g.classList.remove("hide")
+}
+let v3_g=document.getElementById("ic9")
+let v3_r=document.getElementById("ic10")
+let c3_g=document.getElementById("ic11")
+let c3_gr=document.getElementById("ic12")
+c3_g.addEventListener("click",() =>{
+    n--;
+    num.innerText = n;
+})
+c3_gr.addEventListener("click",() =>{
+    n++;
+    num.innerText = n;
+})
+
+v3_g.onclick=function(){
+    v3_g.classList.add("hide")
+    v3_r.classList.remove("hide")
+}
+v3_r.onclick=function(){
+    v3_r.classList.add("hide")
+    v3_g.classList.remove("hide")
+}
+c3_g.onclick=function(){
+    c3_g.classList.add("hide")
+    c3_gr.classList.remove("hide")
+}
+c3_gr.onclick=function(){
+    c3_gr.classList.add("hide")
+    c3_g.classList.remove("hide")
+}
+let v4_g=document.getElementById("ic13")
+let v4_r=document.getElementById("ic14")
+let c4_g=document.getElementById("ic15")
+let c4_gr=document.getElementById("ic16")
+c4_g.addEventListener("click",() =>{
+    n--;
+    num.innerText = n;
+})
+c4_gr.addEventListener("click",() =>{
+    n++;
+    num.innerText = n;
+})
+v4_g.onclick=function(){
+    v4_g.classList.add("hide")
+    v4_r.classList.remove("hide")
+}
+v4_r.onclick=function(){
+    v4_r.classList.add("hide")
+    v4_g.classList.remove("hide")
+}
+c4_g.onclick=function(){
+    c4_g.classList.add("hide")
+    c4_gr.classList.remove("hide")
+}
+c4_gr.onclick=function(){
+    c4_gr.classList.add("hide")
+    c4_g.classList.remove("hide")
+}
+
+let v9_g=document.getElementById("ic33")
+let v9_r=document.getElementById("ic34")
+let c9_g=document.getElementById("ic35")
+let c9_gr=document.getElementById("ic36")
+c9_g.addEventListener("click",() =>{
+    n--;
+    num.innerText = n;
+})
+c9_gr.addEventListener("click",() =>{
+    n++;
+    num.innerText = n;
+})
+v9_g.onclick=function(){
+    v9_g.classList.add("hide")
+    v9_r.classList.remove("hide")
+}
+v9_r.onclick=function(){
+    v9_r.classList.add("hide")
+    v9_g.classList.remove("hide")
+}
+c9_g.onclick=function(){
+    c9_g.classList.add("hide")
+    c9_gr.classList.remove("hide")
+}
+c9_gr.onclick=function(){
+    c9_gr.classList.add("hide")
+    c9_g.classList.remove("hide")
+}
+let v10_g=document.getElementById("ic37")
+let v10_r=document.getElementById("ic38")
+let c10_g=document.getElementById("ic39")
+let c10_gr=document.getElementById("ic40")
+
+c10_g.addEventListener("click",() =>{
+    n--;
+    num.innerText = n;
+})
+c10_gr.addEventListener("click",() =>{
+    n++;
+    num.innerText = n;
+})
+v10_g.onclick=function(){
+    v10_g.classList.add("hide")
+    v10_r.classList.remove("hide")
+}
+v10_r.onclick=function(){
+    v10_r.classList.add("hide")
+    v10_g.classList.remove("hide")
+}
+c10_g.onclick=function(){
+    c10_g.classList.add("hide")
+    c10_gr.classList.remove("hide")
+}
+c10_gr.onclick=function(){
+    c10_gr.classList.add("hide")
+    c10_g.classList.remove("hide")
+}
+let v11_g=document.getElementById("ic41")
+let v11_r=document.getElementById("ic42")
+let c11_g=document.getElementById("ic43")
+let c11_gr=document.getElementById("ic44")
+
+c11_g.addEventListener("click",() =>{
+    n--;
+    num.innerText = n;
+})
+c11_gr.addEventListener("click",() =>{
+    n++;
+    num.innerText = n;
+})
+v11_g.onclick=function(){
+    v11_g.classList.add("hide")
+    v11_r.classList.remove("hide")
+}
+v11_r.onclick=function(){
+    v11_r.classList.add("hide")
+    v11_g.classList.remove("hide")
+}
+c11_g.onclick=function(){
+    c11_g.classList.add("hide")
+    c11_gr.classList.remove("hide")
+}
+c11_gr.onclick=function(){
+    c11_gr.classList.add("hide")
+    c11_g.classList.remove("hide")
+}
+let v12_g=document.getElementById("ic45")
+let v12_r=document.getElementById("ic46")
+let c12_g=document.getElementById("ic47")
+let c12_gr=document.getElementById("ic48")
+
+c12_g.addEventListener("click",() =>{
+    n--;
+    num.innerText = n;
+})
+c12_gr.addEventListener("click",() =>{
+    n++;
+    num.innerText = n;
+})
+v12_g.onclick=function(){
+    v12_g.classList.add("hide")
+    v12_r.classList.remove("hide")
+}
+v12_r.onclick=function(){
+    v12_r.classList.add("hide")
+    v12_g.classList.remove("hide")
+}
+c12_g.onclick=function(){
+    c12_g.classList.add("hide")
+    c12_gr.classList.remove("hide")
+}
+c12_gr.onclick=function(){
+    c12_gr.classList.add("hide")
+    c12_g.classList.remove("hide")
+}
